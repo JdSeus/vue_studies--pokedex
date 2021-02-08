2d8323773af70a224507cc2afa84a3257b307517
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-08 16:28:19 -0300

    sistema de busca

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div id="app">
 
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke,index) in pokemons" :key="index">
-        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+
+      <h1>Pokedex</h1>
+      <input class="input is-rounded" type="text" placeholder="Buscar pokémon pelo nome" v-model="busca">
+      <button id="buscaBtn" class="button is-fullwidth is-success" @click="buscar">Buscar</button>
+      <hr>
+
+      <div v-for="(poke) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="poke.url | pokenumber"/>
       </div>
     </div>
 
@@ -18,18 +24,48 @@ export default {
   name: 'App',
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function() {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&&offset=0").then(res => {
       this.pokemons = res.data.results;
-      console.log(this.pokemons);
+      this.filteredPokemons = res.data.results;
     });
   },
   components: {
     Pokemon
+  },
+  methods: {
+    buscar: function() {
+      this.filteredPokemons = this.pokemons;
+      if (this.busca == '' || this.busca == ' ') {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  computed: {
+    /*
+    resultadoBusca: function() {
+      if (this.busca == '' || this.busca == ' ') {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }*/
+  },
+  filters: {
+    pokenumber: function(value) {
+      var pokenumber = value.split("/");
+      var number = pokenumber.length - 2;
+      return parseInt(pokenumber[number]);  
+    }
   }
+  
 }
 </script>
 
@@ -41,5 +77,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn {
+  margin-top: 2%;
 }
 </style>
